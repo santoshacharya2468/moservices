8afd46aea0eed89e7ae84cda1a6eb7f77c044af1
santosh <santoshacharya2468@gmail.com>
2020-07-02 16:17:42 +0545

    make it betterf

--- routes/adminDashBoard.js
+++ routes/adminDashBoard.js
@@ -2,13 +2,13 @@ const express=require("express");
 const router=express.Router();
 const Category=require("../models/category");
 const Like=require("../models/like");
-const Click=require("../models/click");
+const Visit=require("../models/view");
 const Shop=require("../models/shop");
 router.get("/dashboard",async(req,res)=>{
     try{
         var categoryCount=await Category.count();
         var likeCount=await Like.count();
-        var clickCount=await Click.count();
+        var clickCount=await Visit.count();
         var shopCount=await Shop.count();
         res.json({categories:categoryCount,likes:likeCount,clicks:clickCount,providers:shopCount});
     }
