0c6f9563785634df7d1800de270a788bb65b73d3
santosh acharya <39704996+santoshacharya2468@users.noreply.github.com>
2020-08-10 18:06:16 +0545

    Update view.js

--- models/view.js
+++ models/view.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 const visitSchema=mongoose.Schema({
     visitDate:{
         type:Date,
-        default:Date.now(),
+        default:Date.now,
     }
 });
 module.exports=mongoose.model("Visit",visitSchema);
